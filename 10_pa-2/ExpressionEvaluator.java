8b5a7ee9c947fd948862912b6559e12253dc4f99
darinz <95009007+darinz@users.noreply.github.com>
2025-07-20 23:14:35 -0700

    Create ExpressionEvaluator.java

---
+++ 10_pa-2/ExpressionEvaluator.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class ExpressionEvaluator {
+    public static void main(String[] args) {
+        // TODO: Read input from file or standard input
+        // Example: String expression = "3 + 4 * 2 / ( 1 - 5 )";
+        // You may use Scanner or BufferedReader
+    }
+
+    // Converts infix expression to postfix (RPN)
+    public static Queue<String> infixToPostfix(String expression) throws InvalidExpressionException {
+        // TODO: Implement using Stack and Queue
+        // Return a Queue representing the postfix expression
+        return null;
+    }
+
+    // Evaluates a postfix expression
+    public static int evaluatePostfix(Queue<String> postfix) throws InvalidExpressionException {
+        // TODO: Implement using Stack
+        return 0;
+    }
+
+    // Custom exception for invalid expressions
+    public static class InvalidExpressionException extends Exception {
+        public InvalidExpressionException(String message) {
+            super(message);
+        }
+    }
+} 
